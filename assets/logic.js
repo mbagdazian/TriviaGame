a3e138cca65c1a3887d48fd11bae1a170fa51401
ewok1138 <mbagdazian90@gmail.com>
2017-09-02 07:59:37 -0700

    HW5

---
+++ assets/logic.js
@@ -0,0 +1,30 @@
+
+  	
+$(document).ready(function(){	
+	$('#row2').hide();
+	$('#row3').hide();
+	$('#row4').hide();
+
+$('button').click(function(){
+
+	$('#row2').show();
+
+})
+
+
+
+	})
+
+
+
+
+//start timer button - makes questions appeear starts a two minute count down. 
+
+//create timer (three minutes)
+
+//change points displayed at the end of the timer.
+
+
+//done button displays results page
+
+//relation - points to answers (each correct answer adds a point ---incorrect answers don't count)
